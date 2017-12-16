3b03c050fa6e01b7cb1cb6b8912e2d5554eade28
zTsugumi <phieudieuhai@gmail.com>
2017-12-16 17:06:03 +0300

    Add files via upload

---
+++ implementation.py
@@ -0,0 +1,120 @@
+import heapq
+
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start)  # optional
+    # path.reverse()  # optional
+    return path
+
+
+def draw_tile(graph, ID, width, style):
+    r = "."
+    if 'path' in style and ID in style['path']:
+        r = "@"
+    if 'start' in style and ID == style['start']:
+        r = "A"
+    if 'goal' in style and ID == style['goal']:
+        r = "Z"
+    if ID in graph.walls:
+        r = "#" * width
+    return r
+
+
+def draw_grid(graph, width=2, **style):
+    for y in range(graph.height):
+        for x in range(graph.width):
+            print("%%-%ds" % width % draw_tile(graph, (x, y), width, style), end="")
+        print()
+
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, ID):
+        (x, y) = ID
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, ID):
+        return ID not in self.walls
+
+    def neighbors(self, ID):
+        (x, y) = ID
+        results = [(x + 1, y), (x, y - 1), (x - 1, y), (x, y + 1)]
+        if (x + y) % 2 == 0:
+            results.reverse()  # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, to_node):
+        return self.weights.get(to_node, 1)
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    cameFrom = {}
+    costSoFar = {}
+    cameFrom[start] = None
+    costSoFar[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = costSoFar[current] + graph.cost(next)
+            if next not in costSoFar or new_cost < costSoFar[next]:
+                costSoFar[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                cameFrom[next] = current
+
+    return cameFrom, costSoFar
+
+
+diagram4 = GridWithWeights(10, 10)
+diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
+                                       (4, 3), (4, 4), (4, 5), (4, 6),
+                                       (4, 7), (4, 8), (5, 1), (5, 2),
+                                       (5, 3), (5, 4), (5, 5), (5, 6),
+                                       (5, 7), (5, 8), (6, 2), (6, 3),
+                                       (6, 4), (6, 5), (6, 6), (6, 7),
+                                       (7, 3), (7, 4), (7, 5)]}
